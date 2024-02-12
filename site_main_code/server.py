fd5680e41ff64644cf14f9223abb9705d880eaac
nonamefo <bariseva24@gmail.com>
2024-02-12 23:28:50 +0300

    изминение структуры проекта создание оделного пакета и разбитие программного кода на несколько частей пакет создан так чтобы в будующем можно было без проблем добавить базу данных и чтобы код был читабельным и в нем легко было ореинтироватся

---
+++ site_main_code/server.py
@@ -0,0 +1,84 @@
+from flask import request, render_template
+
+from site_main_code import app
+
+
+@app.route('/', methods=['POST', 'GET'])
+@app.route('/home')
+def home():
+    if request.method == "POST":
+        try:
+            name = request.form["name"]
+            email = request.form["email"]
+            password = request.form["password"]
+            print(name, email, password)
+            return render_template("home.html")
+        except KeyError:
+            try:
+                name = request.form["name"]
+                password = request.form["password"]
+                print(name, password)
+                return render_template("home.html")
+            except Exception as ex:
+                return render_template("error.html")
+        except Exception as ex:
+            return render_template("error.html")
+    else:
+        return render_template("home.html", title="Дом милый дом")
+
+
+@app.route('/tests/<num>')
+def tests(num):
+    return render_template("super_tesks.html", lst=open(f"{num}").readlines())
+
+
+@app.route('/answer', methods=["POST"])
+def answer_test():
+    if request.method == "POST":
+        try:
+            name = request.form["name"]
+            email = request.form["email"]
+            password = request.form["password"]
+            print(name, email, password)
+            return render_template("home.html")
+        except KeyError:
+            try:
+                name = request.form["name"]
+                password = request.form["password"]
+                print(name, password)
+                return render_template("home.html")
+            except Exception as ex:
+                return render_template("error.html")
+        except Exception as ex:
+            return render_template("error.html")
+    else:
+        return render_template("home.html", title="Дом милый дом")
+
+
+@app.route('/enter')
+def enter():
+    return render_template("entry.html")
+
+
+@app.route('/te', methods=["POST"])
+def test_te():
+    name = request.form.get('name')
+    word = request.form.get('word')
+    return f"{name} + {word}"
+@app.route('/regester')
+def regester():
+    return render_template('regestor.html')
+
+
+@app.route('/about_us')
+def about_us():
+    return render_template("about_us.html")
+
+
+@app.route('/error')
+def error():
+    return render_template('error.html')
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000)
