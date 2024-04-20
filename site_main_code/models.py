88295d30d914ba4bdc6cc674ffe24af41f1216e6
Всеволод Бари <bariseva24@gmail.com>
2024-04-20 22:59:31 +0300

    добавление подписей и создание файла requirements.txt

--- site_main_code/models.py
+++ site_main_code/models.py
@@ -1,11 +1,12 @@
 from site_main_code.database import regustration, get_by_name, get_by_cookie_name, get_user_name_password
 from site_main_code import requests, API_key, link_API
+# импортирование необходимых библиотек функций и переменных
 
-
+# получение id пользователя по имени и паролю
 def get_id_by_name(nickname: str, password: str):
     return get_by_name(nickname, password)
 
-
+# создание пользователя с получением данных
 def create_user(email: str, nickname: str, password: str):
     return regustration(email, nickname, password)
 
